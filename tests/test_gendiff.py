f2f8c3443fb62d45579f2746aef87a019fc35002
Polina017 <kapkaeva017@yandex.ru>
2022-06-11 14:53:11 +0300

    step 3 in progress

---
+++ tests/test_gendiff.py
@@ -0,0 +1,18 @@
+import pytest
+import os
+from gendiff.gendiff import generate_diff
+
+
+@pytest.mark.parametrize(
+    'style, file_before, file_after, file_result',
+    [
+        ('plain', 'tests/fixtures/file1.json', 'tests/fixtures/file2.json', 'tests/fixtures/result_plain.txt'),  # noqa:E501
+        ('plain', 'tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml', 'tests/fixtures/result_plain.txt'),  # noqa:E501
+        ('stylish', 'tests/fixtures/file1.json', 'tests/fixtures/file2.json', 'tests/fixtures/result_stylish.txt'),  # noqa:E501
+        ('stylish', 'tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml', 'tests/fixtures/result_stylish.txt'),  # noqa:E501
+    ],
+)
+def test_generate_diff(style, file_before, file_after, file_result):
+    with open(os.path.abspath(file_result)) as f:
+        result = f.read()
+    assert generate_diff(file_before, file_after, style) == result
