98696d0b0a63073f443f9720d7176467093f13c2
Polina017 <kapkaeva017@yandex.ru>
2022-06-11 14:40:50 +0300

    step 3 in progress

--- gendiff/code_gendiff.py
+++ gendiff/code_gendiff.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
+
 import argparse
-parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')  # noqa: E501
 parser.add_argument('first_file', type=str)
 parser.add_argument('second_file', type=str)
 args = parser.parse_args()
