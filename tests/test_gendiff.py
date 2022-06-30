150375beeb07a119bc7d09a1e78edab170d8a4d8
Polina017 <96353351+Polina017@users.noreply.github.com>
2022-06-30 18:40:15 +0300

    Update test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,19 +1,48 @@
-import pytest
-import os
 from gendiff.gen_diff import generate_diff
 
 
-@pytest.mark.parametrize(
-    'style, file_before, file_after, file_result',
-    [
-        ('plain', 'tests/fixtures/file1.json', 'tests/fixtures/file2.json', 'tests/fixtures/result_plain.txt'),  # noqa:E501
-        ('plain', 'tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml', 'tests/fixtures/result_plain.txt'),  # noqa:E501
-        ('stylish', 'tests/fixtures/file1.json', 'tests/fixtures/file2.json', 'tests/fixtures/result_stylish.txt'),  # noqa:E501
-        ('stylish', 'tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml', 'tests/fixtures/result_stylish.txt'),  # noqa:E501
-    ],
-)
-def test_generate_diff(style, file_before, file_after, file_result):
-    with open(os.path.abspath(file_result)) as f:
-        result = f.read()
-    assert generate_diff(file_before, file_after, style) == result
-    
+def test_generate_diff_flat_json():
+    result = generate_diff('./tests/fixtures/file1.json', './tests/fixtures/file2.json')
+    with open('./tests/fixtures/result.txt') as f:
+        assert result == f.read()
+
+
+def test_generate_diff_flat_yaml():
+    result = generate_diff('./tests/fixtures/file1.yaml', './tests/fixtures/file2.yaml')
+    with open('./tests/fixtures/result.txt') as f:
+        assert result == f.read()
+
+
+def test_generate_diff_unsopported():
+    result = generate_diff('./tests/fixtures/file1.yaml', './tests/fixtures/file2.json')
+    assert result == 'File extension is not supported'
+
+
+def test_generate_diff_json_tree():
+    result = generate_diff('./tests/fixtures/file1.2.json', './tests/fixtures/file2.2.json')
+    with open('./tests/fixtures/result2.txt') as f:
+        assert result == f.read()
+
+
+def test_generate_diff_yaml_tree():
+    result = generate_diff('./tests/fixtures/file1.2.yaml', './tests/fixtures/file2.2.yaml')
+    with open('./tests/fixtures/result2.txt') as f:
+        assert result == f.read()
+
+
+def test_generate_diff_json_plain():
+    result = generate_diff('./tests/fixtures/file1.2.json', './tests/fixtures/file2.2.json', 'plain')
+    with open('./tests/fixtures/result_plain.txt') as f:
+        assert result == f.read()
+
+
+def test_generate_diff_json_json():
+    result = generate_diff('./tests/fixtures/file1.2.json', './tests/fixtures/file2.2.json', 'json').strip()
+    with open('./tests/fixtures/result_json.txt') as f:
+        assert result == f.read()
+
+
+def test_generate_diff_json_plain_with_number():
+    result = generate_diff('./tests/fixtures/file1.3.json', './tests/fixtures/file2.3.json', 'plain')
+    with open('./tests/fixtures/result_plain2.txt') as f:
+        assert result == f.read()
